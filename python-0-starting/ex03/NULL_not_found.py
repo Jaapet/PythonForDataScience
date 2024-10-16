4d96dad52a15bcc86d122af16de7d00996e49936
Noé Desprez <noe84starlight@gmail.com>
2024-10-16 13:28:19 +0200

    ex03

--- python-0-starting/ex03/NULL_not_found.py
+++ python-0-starting/ex03/NULL_not_found.py
@@ -1,2 +1,18 @@
+import math
 def NULL_not_found(object: any) -> int:
-	
+	if object is None:
+		print(f"Nothing: {object} {type(object)}")
+	elif object == 0 and type(object) is int:
+		print(f"Zero: {object} {type(object)}")
+	elif isinstance(object, float) and math.isnan(object):
+		print(f"Cheese: {object} {type(object)}")
+	elif object == '':
+		print(f"Empty: {object} {type(object)}")
+	elif type(object) is str and object == "":
+		print(f"String: {object}")
+	elif not object and type(object) is bool:
+		print(f"Fake: {object} {type(object)}")
+	else:
+		print("Type not Found")
+		return 1
+	return 0
