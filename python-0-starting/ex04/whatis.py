e50ae05de538129f2ff7bf08836376a673c8f2fd
Noé Desprez <noe84starlight@gmail.com>
2024-10-19 15:19:58 +0200

    ex04

---
+++ python-0-starting/ex04/whatis.py
@@ -0,0 +1,19 @@
+import sys
+
+try:
+	if len(sys.argv) <= 1:
+		print(f"")
+	elif len(sys.argv) > 2:
+		raise AssertionError("more than one argument is provided")
+	else:
+		try:
+			args = sys.argv[1:]
+			n = int(args[0])
+			if n % 2 == 0:
+				print(f"I'm Even.")
+			else:
+				print(f"I'm Odd.")
+		except:
+			raise AssertionError("argument is not an integer")
+except AssertionError as e:
+	print(f"{AssertionError.__name__}: {e}")
