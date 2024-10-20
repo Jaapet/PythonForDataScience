d1d70aa511b0f46cdbec4821b683811477fe0d63
Noé Desprez <noe84starlight@gmail.com>
2024-10-20 13:30:46 +0200

    a

---
+++ python-0-starting/ex09/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_package',
+    version='0.0',
+    packages=find_packages(),
+    description='A test package',
+    author='ndesprez',
+    author_email='ndesprez@student.42nice.fr',
+    url='https://https://github.com/Jaapet/PythonForDataScience/python-0-starting/ex09/ft_package',
+    python_requires='>=3.6',  # Specify your Python version requirement
+)
