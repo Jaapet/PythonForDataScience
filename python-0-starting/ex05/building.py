48344041d3cb68511f3e57089a723f7869ce6199
Noé Desprez <noe84starlight@gmail.com>
2024-10-20 10:38:18 +0200

    fix

--- python-0-starting/ex05/building.py
+++ python-0-starting/ex05/building.py
@@ -2,6 +2,17 @@ import sys
 import string
 
 def count(str):
+	"""
+    str (input string): The text to analyze
+
+    Prints the following information:
+    1. The number of characters in str
+    2. The number of upper case letters
+    3. The number of lower case letters
+    4. The number of punctuation marks
+    5. The number of spaces
+    6. The number of digits
+    """
 	print(f"The text contains {len(str)} characters:")
 	print(f"{sum(1 for char in str if char.isupper())} lower letters")
 	print(f"{sum(1 for char in str if char.islower())} upper letters")
@@ -10,6 +21,12 @@ def count(str):
 	print(f"{sum(1 for char in str if char.isdigit())} digits")
 
 def main():
+	"""
+    Analyzes the given text and prints information about its content.
+
+    Asks for input if no argument is given,
+    raises AssertionError: Too many arguments provided if more than 1 argument.
+    """
 	try:
 		if len(sys.argv) < 2:
 			try:
